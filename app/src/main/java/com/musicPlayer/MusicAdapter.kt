a01961986f1ce99865bb4244ad194bc30a89ed88
pscommune <daniil.mukhametov.96@mail.ru>
2023-04-10 21:26:06 +0500

    themes are not working correctly, there is no api///

---
+++ app/src/main/java/com/musicPlayer/MusicAdapter.kt
@@ -0,0 +1,136 @@
+package com.musicPlayer
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.text.SpannableStringBuilder
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.text.bold
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.musicPlayer.R
+import com.musicPlayer.MusicAdapter.MyHolder
+import com.musicPlayer.databinding.DetailsViewBinding
+import com.musicPlayer.databinding.MoreFeaturesBinding
+import com.musicPlayer.databinding.MusicViewBinding
+
+class MusicAdapter(private val context: Context, private var musicList: ArrayList<Music>, private val playlistDetails: Boolean = false,
+private val selectionActivity: Boolean = false)
+    : RecyclerView.Adapter<MyHolder>() {
+
+    class MyHolder(binding: MusicViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        val title = binding.songNameMV
+        val album = binding.songAlbumMV
+        val image = binding.imageMV
+        val duration = binding.songDuration
+        val root = binding.root
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        return MyHolder(MusicViewBinding.inflate(LayoutInflater.from(context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.title.text = musicList[position].title
+        holder.album.text = musicList[position].album
+        holder.duration.text = formatDuration(musicList[position].duration)
+        Glide.with(context)
+            .load(musicList[position].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.app_icon).centerCrop())
+            .into(holder.image)
+
+        //for play next feature
+        if(!selectionActivity)
+            holder.root.setOnLongClickListener {
+                val customDialog = LayoutInflater.from(context).inflate(R.layout.more_features, holder.root, false)
+                val bindingMF = MoreFeaturesBinding.bind(customDialog)
+                val dialog = MaterialAlertDialogBuilder(context).setView(customDialog)
+                    .create()
+                dialog.show()
+                dialog.window?.setBackgroundDrawable(ColorDrawable(0x99000000.toInt()))
+
+                bindingMF.AddToPNBtn.setOnClickListener {
+                    try {
+                        if(PlayNext.playNextList.isEmpty()){
+                            PlayNext.playNextList.add(PlayerActivity.musicListPA[PlayerActivity.songPosition])
+                            PlayerActivity.songPosition = 0
+                        }
+
+                        PlayNext.playNextList.add(musicList[position])
+                        PlayerActivity.musicListPA = ArrayList()
+                        PlayerActivity.musicListPA.addAll(PlayNext.playNextList)
+                    }catch (e: Exception){
+                        Snackbar.make(context, holder.root,"Play A Song First!!", 3000).show()
+                    }
+                    dialog.dismiss()
+                }
+
+                return@setOnLongClickListener true
+            }
+
+        when{
+            playlistDetails ->{
+                holder.root.setOnClickListener {
+                    sendIntent(ref = "PlaylistDetailsAdapter", pos = position)
+                }
+            }
+            selectionActivity ->{
+                holder.root.setOnClickListener {
+                    if(addSong(musicList[position]))
+                        holder.root.setBackgroundColor(ContextCompat.getColor(context, R.color.cool_pink))
+                    else
+                        holder.root.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+
+                }
+            }
+            else ->{
+                holder.root.setOnClickListener {
+                when{
+                    MainActivity.search -> sendIntent(ref = "MusicAdapterSearch", pos = position)
+                    musicList[position].id == PlayerActivity.nowPlayingId ->
+                        sendIntent(ref = "NowPlaying", pos = PlayerActivity.songPosition)
+                    else->sendIntent(ref="MusicAdapter", pos = position) } }
+        }
+
+         }
+    }
+
+    override fun getItemCount(): Int {
+        return musicList.size
+    }
+
+    fun updateMusicList(searchList : ArrayList<Music>){
+        musicList = ArrayList()
+        musicList.addAll(searchList)
+        notifyDataSetChanged()
+    }
+    private fun sendIntent(ref: String, pos: Int){
+        val intent = Intent(context, PlayerActivity::class.java)
+        intent.putExtra("index", pos)
+        intent.putExtra("class", ref)
+        ContextCompat.startActivity(context, intent, null)
+    }
+    private fun addSong(song: Music): Boolean{
+        PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.forEachIndexed { index, music ->
+            if(song.id == music.id){
+                PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.removeAt(index)
+                return false
+            }
+        }
+        PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist.add(song)
+        return true
+    }
+    fun refreshPlaylist(){
+        musicList = ArrayList()
+        musicList = PlaylistActivity.musicPlaylist.ref[PlaylistDetails.currentPlaylistPos].playlist
+        notifyDataSetChanged()
+    }
+}
